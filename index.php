4f270f1a40aa80a72cbe685678fb631adf402456
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-17 15:04:41 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+		<title>HOME</title>
+		<link rel="stylesheet" type = "text/css" href = "css/style.css"/>
+	</head>
+	<body>
+		
+    <div class = "header"> <p><font size = "12"> Kyzier</font></p></div>
+			
+			<div class = "body2">
+         <p><font size = "6">Hello & Welcome</font></p>
+         <p><font size = "6">Here are some of my learnings about web animation</font></p>
+        <div class = "center">
+          <p><font size = "6">HOME</font></p>
+        </div>
+        <div class="row cf"> 
+          <div class="six col">
+        <a href="index.php" class="button" id="button-8">HOME</a>
+      </div>
+      </div>
+			<div class = "right">
+        <p><font size = "6"> CANVAS & ABOUT ME </font></p>
+			</div>
+			<div class = "left">
+          <p><font size = "6">CSS & SVG</font></p>
+			</div>
+        <div class="row cf">
+      <div class="six col">
+        <a href="#" class="button" id="button-19">CSS</a>
+      </div>
+      <div class="six col">
+        <a href="#" class="button" id="button-20">SVG</a>
+    </div>
+      </div>
+        <div class="row cf">
+      <div class="six col">
+        <a href="#" class="button" id="button-9">ABOUT ME</a>
+      </div>
+      <div class="six col">
+        <a href="#" class="button" id="button-10">CANVAS</a>
+      </div>
+    </div>
+    </div>
+			<div class = "footer"> 
+			<br>
+			<p><font size = "4">  </font></p>
+			<p><font size = "2"> </font></p>
+			
+			</div>
+		
+	</body>
+</html>
