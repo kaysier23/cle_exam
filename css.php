0c4dcd6bda453f923eecf3982120b15de705c388
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-17 15:22:15 +0800

    Create css.php

---
+++ css.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+		<title>CSS</title>
+		<link rel="stylesheet" type = "text/css" href = "css/style1.css"/>
+	</head>
+	<body>
+		
+		<div class = "header"> </div>
+			<div class = "navi">
+			<a href = "index.php"><font color = "black"> HOME</font></a>
+			<a href = ""><font color = "black"> CSS</font></a>
+			<a href = ""><font color = "black"> SVG</font></a>
+			<a href = ""><font color = "black"> CANVAS</font></a>
+			<a href = ""><font color = "black"> ABOUT ME</font></a>
+			</div>
+			<br>
+			<div class = "body2">
+           <p><font size = "6">CSS</font></p>
+			<div class = "right">
+			</div>
+			<div class = "left">
+			</div>
+			</div>
+			<div class = "footer"> 
+			
+			</div>
+		
+	</body>
+</html>
