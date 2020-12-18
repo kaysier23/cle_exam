85a81301e68d0e5b93a7183b7bea138c6254b1d1
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-19 01:13:15 +0800

    Update scale.php

--- scale.php
+++ scale.php
@@ -1,4 +1,16 @@
 <html>
 <body>
+          <div class="row cf"> 
+          <div class="six col">
+        <a href="css.php" class="button" id="button-8">BACK</a>
+      </div>
+      </div>
+
+<div class="container">
+  <div class="box">SCALE 1</div>
+  <div class="box">SCALE 2</div>
+  <div class="box">SCALE 3</div>
+  <div class="box">SCALE 4</div>
+</div>
 </body>
 </html>
