00a4f798836083c60e2398624b530d27b004e8ae
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-19 00:12:08 +0800

    Create translate.php

---
+++ translate.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<div class="wrap">
+                <div class="main-container">
+                  <h1>0.2s</p>
+                  <div class="box1 box"></div>
+                </div>
+                 <div class="main-container">
+                  <h1>0.4s</p>
+                  <div class="box2 box"></div>
+                </div>
+                 <div class="main-container">
+                  <h1>0.6s</p>
+                  <div class="box3 box"></div>
+                </div>
+              </div>
+</body>
+</html>
