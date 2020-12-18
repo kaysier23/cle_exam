d5725e95e1889f6bfec3f6c375f1d3b9d95a0d58
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-19 00:40:42 +0800

    Update rotate.php

--- rotate.php
+++ rotate.php
@@ -1,11 +1,10 @@
 <html>
 <link rel="stylesheet" type = "text/css" href = "css/style2.css"/>
   <body>
-    <div class="row cf"> 
-          <div class="six col">
-        <a href="index.php" class="button" id="button-8">HOME</a>
-      </div>
-      </div>
-    <div></div>
+<a href="index.php" class="button" id="button-8">HOME</a>
+<div></div>
+<div1></div1>
+<div2></div2>
+<div3></div3>
   </body>
 </html>
