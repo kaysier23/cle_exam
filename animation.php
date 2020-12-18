68d89bec81338b74a241f460d5e40af7708de5fa
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-19 01:46:57 +0800

    Create animation.php

---
+++ animation.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+ <div class="row cf"> 
+          <div class="six col">
+        <a href="css.php" class="button" id="button-8">BACK</a>
+      </div>
+      </div>
+
+<body>
+    <div class="div1" id="one"></div>
+    <div class="div1" id="two"></div>
+    <div class="div2" id="three"></div>
+    <div class="div2" id="four"></div>
+  </body>
+</body>
+</html>
