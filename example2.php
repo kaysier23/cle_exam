37e926c50d0c17703ac5cdb58198ae54784e8e5b
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-19 12:49:13 +0800

    Create example2.php

---
+++ example2.php
@@ -0,0 +1,11 @@
+<html>
+  <body>
+            <div class="row cf"> 
+          <div class="six col">
+        <a href="index.php" class="button" id="button-8">HOME</a>
+  <br><br><br><br>
+  <div id = 'wrapper'>
+    <div id = 'trans'>
+    </div>
+  </div>
+</html>
