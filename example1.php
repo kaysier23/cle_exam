b10f3f9beecf1d323dce4cc5cb8d2b1709c98ef5
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-19 12:35:33 +0800

    Create example1.php

---
+++ example1.php
@@ -0,0 +1,26 @@
+<html>
+<body> 
+<div class="row cf"> 
+          <div class="six col">
+        <a href="css.php" class="button" id="button-8">BACK</a>
+      </div>
+      </div>
+<div class="container">
+  <div class="ex-wrapper">
+    <div class="example one">
+    </div>
+    <span class="tag">translateY(200px)</span>
+  </div>
+  <div class="ex-wrapper">
+    <div class="example two">
+    </div>
+    <span class="tag">translateY(-50px)</span>
+  </div>
+  <div class="ex-wrapper">
+    <div class="example three">
+    </div>
+    <span class="tag">translateX(60px)</span>
+  </div>
+</div>
+</body>
+</html>
