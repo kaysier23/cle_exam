8ea150cb98e5a9d33d7b716e2921b24b121f5ab3
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-19 20:44:43 +0800

    Update canvas.php

--- canvas.php
+++ canvas.php
@@ -16,8 +16,28 @@
 <a href="canvasExample1.php"class="button1">EXAMPLE 1</a>
   <a href="canvasExample2.php"class="button1">EXAMPLE 2</a>
   <a href="canvasExample3.php"class="button1">EXAMPLE 3</a>
+
+<canvas id="animation" width="400" height="400"></canvas>
+        
+<canvas id="animation1" width="400" height="400"></canvas>
+        
 			<div class = "footer"> 
-			
+			 <h3>
+			<a class="insta-footer" href="https://www.facebook.com/pages/Le-Vape-Cafe/585242891871155">
+<div class="insta">
+  <div class="shape"></div>
+</div></a>
+<div class="fb">
+  <div class="facebook fbpos"><a class="fbanchor" href="https://www.facebook.com/pages/Le-Vape-Cafe/585242891871155">F</a></div>
+</div>
+<div class="tw">
+  <div class="twitter twpos"><a class="twanchor" href="https://www.facebook.com/pages/Le-Vape-Cafe/585242891871155">T</a></div>
+</div>
+  </h3>
+  <div class = "TM">
+ 			<p><font size = "1"> Portfolio </font></p>
+			<p><font size = "1">© 2020 / kyzier.LTD / All rights reserved. </font></p>
+    </div>
 			</div>
 		
 	</body>
