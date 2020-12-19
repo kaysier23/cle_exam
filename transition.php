952e667221482682635cc3045a262ed7f3cfb034
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-19 11:34:03 +0800

    Create transition.php

---
+++ transition.php
@@ -0,0 +1,16 @@
+
+<html>
+    <head>
+      <link href="stylesheet.css" rel="stylesheet">
+    </head>	
+	<body>
+            <div class="row cf"> 
+          <div class="six col">
+        <a href="css.php" class="button" id="button-8">BACK</a>
+      </div>
+      </div>
+      <div class="container">
+        <div class="box rotate"></div>
+      </div>
+	</body>	
+</html>
