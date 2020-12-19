8dbddcd927b0fe702cfddf4d72cbd5957a6667e6
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-19 15:11:19 +0800

    Create svg.php

---
+++ svg.php
@@ -0,0 +1,36 @@
+<html>
+<link rel="stylesheet" type = "text/css" href = "css/style1.css"/>
+<body>
+		
+		<div class = "header"> </div>
+			<div class = "navi">
+			<a href = "index.php"><font color = "black"> HOME</font></a>
+			<a href = "css.php"><font color = "black"> CSS</font></a>
+			<a href = "svg.php"><font color = "black"> SVG</font></a>
+			<a href = ""><font color = "black"> CANVAS</font></a>
+			<a href = ""><font color = "black"> ABOUT ME</font></a>
+			</div>
+			<br>
+			<div class = "body2">
+           <p1><font size = "6">SVG</font></p1>
+                <p><font size = "5">  SVG Culminating Learning Evidence</font></p>
+<a href="@keyframes.php"class="button1"> SVG CLE</a>
+			<div class = "right">
+<p><font size = "5">  CSS Transforms & Transitions.</font></p>
+<a href="#"class="button">EXAMPLE 1</a>
+<a href="#" class="button">EXAMPLE 2</a>
+        <p><font size = "5">  CSS Animation With @keyframes</font></p>
+<a href="@keyframes.php"class="button">EXAMPLE 1</a>
+			</div>
+			<div class = "left">
+        <p><font size = "5"> Graphical Art By Combining The SVG Elements</font></p>
+        <a href="svgexample1.php" class="button">SVG Combination</a>
+
+			</div>
+			</div>
+			<div class = "footer"> 
+			
+			</div>
+		
+	</body>
+  </html>
