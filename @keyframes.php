c1e6919b3c2c606b2725e48b9055b0231ac6ec8e
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-19 12:59:10 +0800

    Create @keyframes.php

---
+++ @keyframes.php
@@ -0,0 +1,15 @@
+   <body>
+<div class="row cf"> 
+          <div class="six col">
+        <a href="css.php" class="button" id="button-8">BACK</a>
+                  </div>
+      </div>
+<div class="container">
+  <div class="cube">
+  <div class="side front">front</div>
+  <div class="side right">right</div>
+  <div class="side left">left</div>
+  <div class="side behind">behind</div>
+</div>
+</div>
+</body>
