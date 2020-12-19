b3450dc4c25d6e192c6f2d23e6a76a859aacd84b
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-19 16:56:17 +0800

    Create canvas.php

---
+++ canvas.php
@@ -0,0 +1,24 @@
+	<html>
+  <link rel="stylesheet" type = "text/css" href = "css/style16.css"/>
+<body>	
+		<div class = "header"> </div>
+			<div class = "navi">
+			<a href = "index.php"><font color = "black"> HOME</font></a>
+			<a href = "css.php"><font color = "black"> CSS</font></a>
+			<a href = "svg.php"><font color = "black"> SVG</font></a>
+			<a href = "canvas.php"><font color = "black"> CANVAS</font></a>
+			<a href = "aboutme.php"><font color = "black"> ABOUT ME</font></a>
+			</div>
+			<br>
+			<div class = "body2">
+           <p1><font size = "6">CANVAS</font></p1>
+                <p><font size = "5">  Graphical Arts using HTML5 Canvas Drawing properties</font></p>
+<a href="canvasExample1"class="button1">EXAMPLE 1</a>
+  <a href="#"class="button1">EXAMPLE 2</a>
+ <a href="translate.php" class="button1">EXAMPLE 3</a>
+			<div class = "footer"> 
+			
+			</div>
+		
+	</body>
+  </html>
